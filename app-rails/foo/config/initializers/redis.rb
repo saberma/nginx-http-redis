24d2b3f29dcc038c9d751ba50cacfdfab6784d71
saberma <mahb45@gmail.com>
2015-09-28 18:28:26 +0800

    config redis.

--- app-rails/foo/config/initializers/redis.rb
+++
@@ -1,3 +0,0 @@
-require "redis"
-
-$redis = Redis.new(host: "redis", port: 6379, db: 10)
--- app-rails/foo/config/initializers/redis_page.rb
+++ app-rails/foo/config/initializers/redis_page.rb
@@ -1,6 +1,7 @@
-# Foo::Application.configure do
+require "redis"
+
 RedisPage.configure do |config|
   # 通过访问 http://cache:ewHN84JZLyRurX@example.com:8081/products/1 来刷新缓存
   config.sweeper = { port: 8081, username: 'cache', password: 'ewHN84JZLyRurX' }
-  config.redis = $redis
+  config.redis = Redis.new(host: "redis", port: 6379, db: 10)
 end
