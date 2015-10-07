daa701c09dba5f89637afec247939179913e32d1
saberma <mahb45@gmail.com>
2015-10-07 21:09:06 +0800

    add ttl.

--- app-rails/foo/config/initializers/redis_page.rb
+++ app-rails/foo/config/initializers/redis_page.rb
@@ -4,4 +4,5 @@ RedisPage.configure do |config|
   # 通过访问 http://cache:ewHN84JZLyRurX@example.com:8081/products/1 来刷新缓存
   config.sweeper = { port: 8081, username: 'cache', password: 'ewHN84JZLyRurX' }
   config.redis = Redis.new(host: "redis", port: 6379, db: 11)
+  config.ttl = 604800 # 1 week
 end
